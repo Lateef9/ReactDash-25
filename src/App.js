cbb31e8873c4ae275a298f187d9a3dbc164dfb54
Shaik Lateef <shaiklateef7761@gmail.com>
2024-04-20 23:48:26 +0530

    Task-4 [ImageSlider]

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import ImageSlider from "./component/imageSlider";
 
-import './App.css';
-import Accordian from './component/Accordion';
-import Randomcolor from './component/Random-color';
-import StarRating from './component/Star-rating';
+// import Accordian from './component/Accordion';
+// import Randomcolor from './component/Random-color';
+// import StarRating from './component/Star-rating';
 function App() {
   return (
     <div className="App">
       {/* <Accordian /> */}
       {/* <Randomcolor/> */}
-      <StarRating/>
+      {/* <StarRating/> */}
+      <ImageSlider url={"https://picsum.photos/v2/list"}  page={"1"} limit={"10"}/>
     </div>
   );
 }
